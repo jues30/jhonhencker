9471e14fa2cf1fb330a1233e68b94da94a8d6c28
jues30 <113048925+jues30@users.noreply.github.com>
2022-10-07 10:42:24 -0500

    Ajustes poemas. Autor de libros.

--- vistas/los_poemas_sin_autor.php
+++ vistas/los_poemas_sin_autor.php
@@ -33,100 +33,66 @@
         <h3>LA DAMA ARROGANTE</h3>
         <p>Ella, consciente de su belleza, declamó lo siguiente:
         <br>"Soy hermosa, tanto que abordo todo el mal,
-        <br>Al punto que el sacerdote perfuma sus sabanas 
-        <br>Al tener mi rostro en su pensamiento.
-        <br>Torno a todos los hombres ignorantes
-        <br>Sólo con mis encantos superficiales.
+        <br>al punto que el sacerdote perfuma sus sabanas 
+        <br>al tener mi rostro en su pensamiento.
+        <br>Vuelvo  a todos los hombres ignorantes
+        <br>sólo con mis encantos superficiales.
         <br>Solos, exhalan un olor a fracaso
-        <br>Con su mirada de azur sobre mis caderas,
-        <br>Vacíos de pensamientos en sus ojos bellos
-        <br>Mezclados con metal a causa de sus fracasos.
+        <br>con su mirada de azur sobre mis caderas,
+        <br>vacíos de pensamientos en sus ojos bellos
+        <br>mezclados con metal a causa de sus fracasos.
         <br>Camino con cabeza bamboleante
-        <br>Como si paseara en una góndola,
-        <br>Recibiendo la atención ardorosa
-        <br>De almas vacías de esperanzas.
+        <br>como si paseara en una góndola,
+        <br>recibiendo la atención ardorosa
+        <br>de almas vacías de esperanzas.
         <br>Y, verduga, llena de estíos y otoños,
-        <br>Aplasto con amabilidad fingida deseos efímeros
-        <br>Rompiendo orgullos de hombres pobres y ricos
-        <br>Y causando lágrimas en infinitos números.
+        <br>aplasto con amabilidad fingida deseos efímeros
+        <br>rompiendo orgullos de hombres pobres y ricos
+        <br>y causando lágrimas en infinitos números.
         <br>Rompo el vuelo de halcones con sonrisas de brea
-        <br>Mirando al cielo como si fuera una diosa,
-        <br>Hirviendo vivos a los infames profetas
-        <br>De una sociedad incrédula de inteligencia,
-        <br>Saturando mi olfato de olores mezclados 
-        <br>Con resina y almizcle, horrendo para mis sentidos
-        <br>Pero rebosantes en los cuerpos de los mortales".
+        <br>mirando al cielo como si fuera una diosa,
+        <br>hirviendo vivos a los infames profetas
+        <br>de una sociedad incrédula de inteligencia,
+        <br>saturando mi olfato de olores mezclados 
+        <br>con resina y almizcle, horrendo para mis sentidos
+        <br>pero rebosantes en los cuerpos de los mortales".
         </p>
         <br></br>
         <h3>GUERRERA</h3>
         <p>
         Bendito seas, mi Dios, que de ella te apiadas,
         <br>Mientras bella, se abre paso en la vida, cual alud en bajada,
-        <br>Y la pasión de su alma se resiste al amo tirano
-        <br>Al tiempo que se despierta con emociones extrañas.
+        <br>y la pasión de su alma se resiste al amo tirano
+        <br>al tiempo que se despierta con emociones extrañas.
         <br>Doblada por el peso de los años, ella no se rinde,
-        <br>Perdiendo la mirada en los muelles y en ríos crecientes,
-        <br>Saturando los sentidos de magia al arrullo del agua,
-        <br>Vive apoyada en lo positivo, como la cúpula de una iglesia.
+        <br>perdiendo la mirada en los muelles y en ríos crecientes,
+        <br>saturando los sentidos de magia al arrullo del agua,
+        <br>vive apoyada en lo positivo, como la cúpula de una iglesia.
         <br>A veces tiene voz temblorosa, cuando el alma se le encoge,
-        <br>Pero bajo el latido de las capitales, se irgue triunfante,
-        <br>Canta fuerte con su corazón puro en todos los rincones,
-        <br>Y casi de inmediato se disipa la soledad de su alma.
+        <br>pero bajo el latido de las capitales, se irgue triunfante,
+        <br>canta fuerte con su corazón puro en todos los rincones,
+        <br>y casi de inmediato se disipa la soledad de su alma.
         <br>Amadísima por sí misma, ¡oh Noche!, ¡oh Corazón!,
         <br>¡oh Vino! Todos tres a sus pies se agitan de pasión.
         <br>Mientras sobre la almohada, a veces frustrada,
-        <br>Toma fuerza entre un sufragio de colores,
-        <br>Gatos furtivos, iras terribles y visiones de ilusión.
+        <br>toma fuerza entre un sufragio de colores,
+        <br>gatos furtivos, iras terribles y visiones de ilusión.
         </p>        
         <br></br>
-        <h3>TÚ BAJO EL ATARCEDER</h3>
-                <p>
-                Saludo el ocaso más glorioso del mundo
-                <br>Al mismo tiempo que escucho al cráneo en cada voluptuosidad.
-                <br>Bañada de luz dorada estaba ella, acostada,
-                <br>Mientras su rostro, bendecido por espíritus,
-                <br>Devora mi cerebro, que pasea como un centinela.
-                <br>Y de aquellos besos profundos, que generan formas amargas,
-                <br>Me llena los labios; mientras las teas, como rosas luminosas
-                <br>Contrastan con el atardecer dorado allá abajo.
-                </p>
-                <p>
-                Entonces me empapo en vino para encontrar las bestias
-                <br>Que palpan en vano la eternidad de la vergüenza y la melancolía,
-                <br>Mientras sobre el viejo océano se engendra la muerte y la vida
-                <br>Y por eso pido: ¡Satán, apiádate de mí larga miseria!
-                <br>Sin embargo, como padre adoptivo de tanta majestad,
-                <br>Hipócrita, pido indulgencia y a la vez me maravillo
-                <br>Al ver cómo me agrada descubrir que, en el mal,
-                <br>Colmado de dolor, se halla toda voluptuosidad.
-                <br>Así que humeo con la noche, mientras el abismo se abre a mis pies,
-                <br>De aspecto renaciente bordeado de metales,
-                <br>Al tiempo que insulto al sol con lágrima entre líneas, 
-                <br>Y mi mirada vive sin potencia bajo la noche de terribles goces.
-                </p>
-                <p>
-                Tus brazos se burlan del espíritu que duerme,
-                <br>Mientras te asemejas a la princesa de las nubes
-                <br>Que frecuenta la nave con tus cavidades sangrientas 
-                <br>De aquellos cuerpos con sus perfumes horrendos.
-                <br>Áspera hacha entre mis ojos, sol de mi natura,
-                <br>¡Oh tú, ángel que encierra mis antojos!
-                <br>Dile a Dios: “¡Con tu voluntad, extrae el sol de su carne pura!”.
-                <br>Ángel lleno de belleza, como te vuelves una alegoría
-                <br>Y mis caros recuerdos de tu corazón,
-                <br>Que en el mar halla bordes de locura,
-                <br>Brumosas estepas y bruñidos campos
-                <br>A la hora de las voluptuosidades que brillan de azul.
-                <br>Tus ojos, que parecen con fervor,
-                <br>Una Magdalena fogosa gratuita con piernas de estatua,
-                <br>Precipitándose en el Infierno cual rayo brillante
-                <br>Como esos espíritus de placeres diamantinos.
-                <br>Mientras el perfume de los verdes tamarindos
-                <br>Que quieren que el mundo te tema
-                <br>Vuela ante la ilusión y la dulzura
-                <br>Que se contempla a si mismo ¡Y siempre te veo!
-                <br>Con ojos embriagados de soles radiantes.
-                </p>
+        <h3>INALCANZABLE</h3>
+        <p>
+        Tú, a quien la noche toma como hija,
+        <br>como esculpida en la gran piedra,
+        <br>te tornas imbatible al deseo mortal.
+        <br>Inalcanzable con tus carnes de niña, 
+        <br>con tus juegos de virgen loca
+        <br>que desesperan a los amantes.
+        <br>Inalcanzable al anciano decrépito,
+        <br>apasionada a las anchas alas de los ángeles,
+        <br>al tiempo que, de tus ojos, tus dulces ojos,
+        <br>jamás llorosos ni irritados,
+        <br>desprendes caos a las soledades profundas.
+        </p>
         <div class="counts">
             <div class="row">
                 <div class="col-lg-3 col-md-6 mt-5 mt-md-0">
